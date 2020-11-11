145f985cd93aecc695aa19248b747ced4d53f3c5
jaronlee <jaronjlee@gmail.com>
2020-11-11 01:29:20 -0800

    edit app js

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+});
+}
+
 //CSV parse 
 const fs = require('fs');
 const parse = require('csv-parse/lib/sync');
